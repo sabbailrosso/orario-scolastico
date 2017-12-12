356a5c8b79ccc74775dda0715ca08a2e1fa2260d
sabbailrosso <34487244+sabbailrosso@users.noreply.github.com>
2017-12-12 19:13:42 +0100

    Add files via upload

---
+++ WindowsApplication1/WindowsApplication1/Form1.vb
@@ -0,0 +1,9 @@
+﻿Public Class Form1
+
+	Private Sub BtnLun1_Click(sender As Object, e As EventArgs) Handles BtnLun1.Click
+		Note.Visible = False
+		Me.Visible = False
+		Show(Note)
+
+	End Sub
+End Class
